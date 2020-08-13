c0e4e4eb7a379eea2200f58b3f588f51b0e760f5
Milana8 <91amilana@gmail.com>
2020-08-13 15:17:55 +0200

    Login-predefined order

---
+++ Nedeljni_II_Milana_Arnautovic/Zadatak_1/ViewModel/LoginViewModel.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+using Zadatak_1.Command;
+using Zadatak_1.View;
+
+namespace Zadatak_1.ViewModel
+{
+    class LoginViewModel : ViewModelBase
+    {
+        LoginView view;
+
+
+        #region Constructors
+
+        public LoginViewModel(LoginView view)
+        {
+            this.view = view;
+
+        }
+        #endregion
+
+        #region Property
+
+        private string userName;
+        public string UserName
+        {
+
+            get
+            {
+                return userName;
+            }
+            set
+            {
+                userName = value;
+                OnPropertyChanged("UserName");
+            }
+        }
+
+        #endregion
+
+        #region Commands
+        private ICommand login;
+        /// <summary>
+        /// Command login
+        /// </summary>
+        public ICommand Login
+        {
+            get
+            {
+                if (login == null)
+                {
+                    login = new RelayCommand(LoginExecute);
+                }
+                return login;
+            }
+            set { login = value; }
+        }
+        /// <summary>
+        /// Method for checking username and password
+        /// </summary>
+        /// <param name="o"></param>
+        private void LoginExecute(object o)
+        {
+
+            try
+            {
+                StreamReader sr = new StreamReader(@"..\..\ClinicAccess.txt");
+                string line = "";
+                List<string> clinic = new List<string>();
+
+                while ((line = sr.ReadLine()) != null)
+                {
+                    clinic.Add(line);
+                }
+                sr.Close();
+                string password = (o as PasswordBox).Password;
+                if (userName == clinic[0] && password == clinic[1])
+                {
+                    AddClinicAdministratorView cl = new AddClinicAdministratorView();
+                    view.Close();
+                    cl.ShowDialog();
+                }
+
+
+                else
+                {
+                    MessageBox.Show("Incorrect username or password. Please try again.");
+                }
+            }
+
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+        }
+
+        #endregion
+    }
+}
