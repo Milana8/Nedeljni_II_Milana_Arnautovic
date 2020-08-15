a54e28ffdd442c1a74f7fa473674fd87a7f01af5
Milana8 <91amilana@gmail.com>
2020-08-15 11:28:19 +0200

    Add Administrator

---
+++ Nedeljni_II_Milana_Arnautovic/Zadatak_1/Service.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Zadatak_1.Model;
+
+namespace Zadatak_1
+{
+    class Service
+    {
+        public List<tblUser> GetAllUsers()
+        {
+            try
+            {
+                using (Nedeljni_IIEntities context = new Nedeljni_IIEntities())
+                {
+                    List<tblUser> list = new List<tblUser>();
+                    list = (from x in context.tblUsers select x).ToList();
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        public List<tblAdministrator> GetAllAdministrators()
+        {
+            try
+            {
+                using (Nedeljni_IIEntities context = new Nedeljni_IIEntities())
+                {
+                    List<tblAdministrator> list = new List<tblAdministrator>();
+                    list = (from x in context.tblAdministrators select x).ToList();
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        public List<vwAdministrator> GetAllAdministratorView()
+        {
+            try
+            {
+                using (Nedeljni_IIEntities context = new Nedeljni_IIEntities())
+                {
+                    List<vwAdministrator> list = new List<vwAdministrator>();
+                    list = (from x in context.vwAdministrators select x).ToList();
+                    return list;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return null;
+            }
+        }
+
+        public bool AddAdministrator(vwAdministrator administrator)
+        {
+            try
+            {
+                using (Nedeljni_IIEntities context = new Nedeljni_IIEntities())
+                {
+                    tblUser user = new tblUser
+                    {
+                        Gender = administrator.Gender,
+                        IdCard = administrator.IdCard,
+                        DateOfBirth = administrator.DateOfBirth,
+                        Citizenship = administrator.Citizenship,
+                        FirstName = administrator.FirstName,
+                        Pasword = administrator.Pasword,
+                        Surname = administrator.Surname,
+                        Username = administrator.Username
+                    };
+                    context.tblUsers.Add(user);
+                    context.SaveChanges();
+                    administrator.UserID = user.UserID;
+                    tblAdministrator newAdministrator = new tblAdministrator
+                    {
+                        UserID = user.UserID,
+                        
+                    };
+                    context.tblAdministrators.Add(newAdministrator);
+                    context.SaveChanges();
+                    administrator.AdministratorID = newAdministrator.AdministratorID;
+                    return true;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine("Exception" + ex.Message.ToString());
+                return false;
+            }
+        }
+    }
+}
--- Nedeljni_II_Milana_Arnautovic/Zadatak_1/ViewModel/AddClinicAdministratorViewModel.cs
+++ Nedeljni_II_Milana_Arnautovic/Zadatak_1/ViewModel/AddClinicAdministratorViewModel.cs
@@ -1,12 +1,159 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using Zadatak_1.Command;
+using Zadatak_1.Model;
+using Zadatak_1.View;
 
 namespace Zadatak_1.ViewModel
 {
     class AddClinicAdministratorViewModel :ViewModelBase
+   
     {
+
+        AddClinicAdministratorView view;
+        Service service = new Service();
+        Validation validation = new Validation();
+
+        public AddClinicAdministratorViewModel(AddClinicAdministratorView view)
+        {
+            this.view = view;
+            administrator = new vwAdministrator();
+            AdminList = service.GetAllAdministratorView().ToList();
+
+        }
+
+        #region Properties
+        private vwAdministrator administrator;
+        public vwAdministrator Administrator
+        {
+            get
+            {
+                return administrator;
+            }
+            set
+            {
+                administrator = value;
+                OnPropertyChanged("Administrator");
+            }
+        }
+        #region Properties
+        private List<vwAdministrator> adminList;
+        public List<vwAdministrator> AdminList
+        {
+            get { return adminList; }
+            set
+            {
+                adminList = value;
+                OnPropertyChanged("AdminList");
+            }
+        }
+
+        
+
+        /// <summary>
+        /// Checks if its possible to execute the add and edit admin commands
+        /// </summary>
+        private bool isUpdateAdmin;
+        public bool IsUpdateAdmin
+        {
+            get
+            {
+                return isUpdateAdmin;
+            }
+            set
+            {
+                isUpdateAdmin = value;
+            }
+        }
+        #endregion
+
+        #endregion
+
+        #region Commands
+        private ICommand save;
+        public ICommand Save
+        {
+            get
+            {
+                if (save == null)
+                {
+                    save = new RelayCommand(param => SaveExecute(), param => CanSaveExecute());
+                }
+                return save;
+            }
+        }
+
+        private ICommand close;
+        public ICommand Close
+        {
+            get
+            {
+                if (close == null)
+                {
+                    close = new RelayCommand(param => CloseExecute(), param => CanCloseExecute());
+                }
+                return close;
+            }
+        }
+
+        #endregion
+
+        #region Functions
+
+        private void SaveExecute()
+        {
+            var result = MessageBox.Show("Are you sure you want to create this admin?", "Confirmation", MessageBoxButton.YesNo, MessageBoxImage.Question);
+
+            if (result == MessageBoxResult.Yes)
+            {
+                try
+                {
+                    service.AddAdministrator(Administrator);
+                    IsUpdateAdmin = true;
+                    service.GetAllAdministratorView().ToList();
+                    MessageBox.Show("You successfully created admin!", "Notification");
+                    view.Close();
+                }
+                catch (Exception ex)
+                {
+                    Debug.WriteLine("Exception" + ex.Message.ToString());
+                }
+            }
+            else
+            {
+                return;
+            }
+        }
+
+
+        private bool CanSaveExecute()
+        { 
+
+             if (Validation.IDCard(Administrator.IdCard) && Validation.Password(Administrator.Pasword))
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+
+    private void CloseExecute()
+        {
+            view.Close();
+        }
+
+        private bool CanCloseExecute()
+        {
+            return true;
+        }
+        #endregion
     }
 }
